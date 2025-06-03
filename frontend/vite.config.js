fc510a90ed1c242a76dbe54fc883e32994d5847f
sariasss <sara.arias085@gmail.com>
2025-06-03 13:08:43 +0200

    dockerignore 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './', // <-- importante si tu app se sirve en subrutas o para evitar errores en producción
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    strictPort: true,
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+  },
 })
