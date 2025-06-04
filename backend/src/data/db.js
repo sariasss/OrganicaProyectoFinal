e27057937eb7a63beae324b66e49017f4588f084
sariasss <sara.arias085@gmail.com>
2025-06-04 17:23:36 +0200

    script funcinal

--- backend/src/data/db.js
+++ backend/src/data/db.js
@@ -1,14 +1,23 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import createData from '../scripts/script.js'; // Asegúrate de que la ruta sea correcta
 
 dotenv.config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        if (process.env.RUN_SEED === 'true') {
+            await createData();
+            console.log("✅ Datos insertados correctamente.");
+        }
+
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`❌ Error: ${error.message}`);
         process.exit(1);
     }
 };
