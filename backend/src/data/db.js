65272c4268b3762930698757e09deba58c989add
sariasss <sara.arias085@gmail.com>
2025-06-04 17:34:02 +0200

    script funcional ahora si ?

--- backend/src/data/db.js
+++ backend/src/data/db.js
@@ -1,23 +1,15 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { createData } from '../scripts/script.js';
 
 dotenv.config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-        if (process.env.RUN_SEED === 'true') {
-            await createData();
-            console.log("✅ Datos insertados correctamente.");
-        }
-
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        await createData();
     } catch (error) {
-        console.error(`❌ Error: ${error.message}`);
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
