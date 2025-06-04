f486813d151da8b5c8ee5fbaf202a123e7df012e
sariasss <sara.arias085@gmail.com>
2025-06-04 02:29:25 +0200

    nos e

--- backend/src/app.js
+++ backend/src/app.js
@@ -46,10 +46,6 @@ app.get("/", (req, res) => {
     res.json({ message: "API funcionando" });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Algo salió mal!' });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       usePolling: true,
     },
      allowedHosts: [
-      'front-production-0596.up.railway.app/'
+      'front-production-0596.up.railway.app'
     ]
   },
 })
